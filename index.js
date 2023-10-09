bc1b0a0d118360e989b9ac3e77cb9d62f7798457
Aya Abu AlHummos <ayaahmad113@gmail.com>
2023-10-09 23:07:36 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,11 +1,37 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(distance){
+let result 
+if (distance <= 400) {
+  result = "This one is on me!"
+}
+else if (distance <= 2000 && distance > 400) {
+  result = "That will be twenty bucks."
+}
+else if (distance > 2000 && distance <= 2500) {
+  result = "I will gladly take your thirty bucks."
+}
+else if (distance > 2500) {
+  result = "No can do."
+}
+return result
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity (city) {
+  return city === 'NYC' ? 'Ok, sounds good.' : 'No go.';
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tip){
+  let say
+  switch (tip) {
+    case "generous" :
+    say = "Thank you so much."
+    break
+    case "not as generous" :
+      say = "Thank you."
+      break
+      default: 
+      say = "Bye."
+      break
+
+  }
+  return say
 }
